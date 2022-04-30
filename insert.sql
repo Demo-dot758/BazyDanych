9e1d8d4eea777ea39667a7a04edd589c3ffcdbef
Demo-dot758 <97907711+Demo-dot758@users.noreply.github.com>
2022-04-30 12:10:46 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -2,3 +2,19 @@ insert all
 into po.literatura values ('abramowitz+stegun','book')
 into po.literatura values ('hicks2001','book')
 select *  from dual;
+
+insert all
+into po.dane values ('abramowitz+stegun','author','Milton Abramowitz and Irene A. Stegun')
+into po.dane values ('abramowitz+stegun','title','Handbook of Mathematical Functions with Formulas, Graphs, and Mathematical Tables"')
+into po.dane values ('abramowitz+stegun','publisher','Dover')
+into po.dane values ('abramowitz+stegun','year','1964')
+into po.dane values ('abramowitz+stegun','address','New York')
+into po.dane values ('abramowitz+stegun','edition','ninth Dover printing, tenth GPO printing')
+into po.dane values ('hicks2001','author','von Hicks, III, Michael')
+into po.dane values ('hicks2001','title','Design of a Carbon Fiber Composite Grid Structure for the GLAST Spacecraft Using a Novel Manufacturing Technique')
+into po.dane values ('hicks2001','publisher','Stanford Press')
+into po.dane values ('hicks2001','year','2001')
+into po.dane values ('hicks2001','address','Palo Alto')
+into po.dane values ('hicks2001','edition','1st')
+into po.dane values ('hicks2001','isbn','0-69-697269-4')
+select *  from dual;
