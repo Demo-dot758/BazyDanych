568535022e16509caf7387b3fa18e07ce69e2770
jmeko1214 <62238227+jmeko1214@users.noreply.github.com>
2022-05-03 21:57:28 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,19 +1,4 @@
 insert all
-into po.literatura values ('abramowitz+stegun','book')
-into po.literatura values ('hicks2001','book')
-into po.literatura values ('King','book')
-into po.literatura values ('Martin','book')
-into po.literatura values ('clarke+g+g+milz','article')
-into po.literatura values ('cl+anal','article')
-into po.literatura values ('tolkien','book')
-into po.literatura values ('rowling','book')
-into po.literatura values ('texas','manual')
-into po.literatura values ('reiman','phdthesis')
-into po.literatura values ('ak-smith','phdthesis')
-into po.literatura values ('jmartindell','phdthesis')
-select *  from dual;
-
-insert all
 into po.dane values ('abramowitz+stegun','author','Milton Abramowitz and Irene A. Stegun')
 into po.dane values ('abramowitz+stegun','title','Handbook of Mathematical Functions with Formulas, Graphs, and Mathematical Tables"')
 into po.dane values ('abramowitz+stegun','publisher','Dover')
